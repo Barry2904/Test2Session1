4088ccf4f70f75e1b4d670a3d5211ba70e045726
Bharath J Ramalingam <96277696+Barry2904@users.noreply.github.com>
2022-02-27 23:59:25 +0000

    done

--- p6original.c
+++ p6original.c
@@ -1,24 +1,36 @@
 #include<stdio.h>
-void input_string(char *a)
+#include<string.h>
+void input_string(char *string)
 {
   printf("Enter a string:- ");
-  scanf("%s",a);
+  scanf("%s",string);
 }
 int str_reverse(char *string,char *substring)
 {
+  int k;
   printf("Enter the substring to be found:- ");
   scanf("%s",substring);
+  printf("%s && %s\n",substring,string);
+  char *i=strstr(string,substring);
+  k=i-string;
+  return k;
 }
 void output(char *string,char *substring,int index)
 {
-  
+  if(index>=0 && index<=200)
+    printf("%s is substring of %s with index %d",substring,string,index);
+  else
+    printf("%s is not substring of %s",substring,string);
+    
 }
 int main()
 {
   int index;
-  char a,string,substring;
-  input_string(&a);
-  index=str_reverse(&string,&substring);
-  output(&string,&substring,index);
+  char string[200],substring[200];
+  input_string(string);
+  index=str_reverse(string,substring);
+  output(string,substring,index);
   return 0;
 }
+
+
